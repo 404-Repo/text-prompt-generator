e8f4c6e0eec37ea3f3bb2581555b8f96aea16a2c
teshaTe <tereshin.tesha@gmail.com>
2024-03-22 16:07:18 +0000

    [UDPATED] small bug fix

--- prompt_generator_tool.py
+++ prompt_generator_tool.py
@@ -10,11 +10,14 @@ if __name__ == '__main__':
     prompt_generator = PromptGenerator.PromptGenerator()
     if args.mode == "online":
         prompt_generator.online_generator()
+        prompt_generator.filter_prompts()
+
     elif args.mode == "offline":
         prompt_generator.offline_generator()
+        prompt_generator.filter_prompts()
+
     elif args.mode == "filter":
         prompt_generator.filter_prompts()
     else:
         raise ValueError(f"Unknown mode was specified: {args.mode}. Supported modes are 'online', 'offline', 'filter'.")
 
-    prompt_generator.filter_prompts()
