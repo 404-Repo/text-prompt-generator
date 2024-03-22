3843666b25e71efc9feb59c8265a2524b07dd841
teshaTe <tereshin.tesha@gmail.com>
2024-03-22 16:06:31 +0000

    [ADDED] bash installation script

---
+++ install_env.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Stop the script on any error
+set -e
+
+# Check for Conda installation and initialize Conda in script
+if [ -z "$(which conda)" ]; then
+    echo "Conda is not installed or not in the PATH"
+    exit 1
+fi
+
+# Attempt to find Conda's base directory and source it (required for `conda activate`)
+CONDA_BASE=$(conda info --base)
+source "${CONDA_BASE}/etc/profile.d/conda.sh"
+
+# Create environment and activate it
+conda env create -f environment.yml
+conda activate three_gen_prompt_generator
+conda info --env
+
+python -m spacy download en_core_web_sm
