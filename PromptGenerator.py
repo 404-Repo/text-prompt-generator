e54c222d28a32a6c96f37c829ef14ddd70edfced
teshaTe <tereshin.tesha@gmail.com>
2024-04-12 11:31:20 +0100

    [UDPATED] updated logger, added llm check of the prompt in generation process

--- PromptGenerator.py
+++ PromptGenerator.py
@@ -6,6 +6,7 @@ import yaml
 import colorama
 import random
 import tqdm
+import logging
 
 import llama_cpp
 import groq
@@ -24,25 +25,41 @@ class PromptGenerator:
     """
     def __init__(self):
         colorama.init()
+        self.__logger = self._init_logger()
         self.__config_data = self.load_config_file()
+
+    """ Initializing custom logger """
+    def _init_logger(self):
+        logger = logging.getLogger("app")
+        logger.setLevel(logging.INFO)
+        handler = logging.StreamHandler(sys.stdout)
+        formatter = logging.Formatter('%(levelname)s:%(message)s')
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        return logger
 
     """ Function that calls Groq api for generating requested output. All supported by Groq models are supported. """
     def online_generator(self):
-        print("\n", "*" * 40)
-        print("[INFO] *** Prompt Dataset Generator ***")
-        print("*" * 40, "\n")
+        print("\n")
+        self.__logger.info("*" * 40)
+        self.__logger.info(" *** Prompt Dataset Generator ***")
+        self.__logger.info("*" * 40)
+        print("\n")
 
         prompt = self._load_input_prompt()
         object_categories = self._load_object_categories()
-        print(f"[INFO] Object categories: {colorama.Fore.GREEN}{object_categories}{colorama.Style.RESET_ALL}")
+        self.__logger.info(f" Object categories: {colorama.Fore.GREEN}{object_categories}{colorama.Style.RESET_ALL}")
 
-        print("[INFO] Started prompt generation.")
+        self.__logger.info(" Started prompt generation.")
+
         t1 = time()
-
         client = groq.Groq(api_key=self.__config_data["groq_api_key"])
 
         for i in range(self.__config_data["iteration_num"]):
-            print(f"\n[INFO] Iteration: {colorama.Fore.GREEN}{i}{colorama.Style.RESET_ALL} \n")
+            print("\n")
+            self.__logger.info(f" Iteration: {colorama.Fore.GREEN}{i}{colorama.Style.RESET_ALL}")
+            print("\n")
+
             prompt_in = copy.copy(prompt)
 
             output_list = []
@@ -73,7 +90,10 @@ class PromptGenerator:
                 prompt_in = prompt_in.replace(category, "member_placeholder")
 
                 # extracting the response of the llm model: generated prompts
-                output_list.append(output.choices[0].message.content)
+                output_prompt = output.choices[0].message.content
+                score = self.check_prompt(output_prompt)
+                if float(score[0]) > 0.5:
+                    output_list.append(output_prompt)
 
             processed_prompts = self.post_process_prompts(output_list)
             checked_prompts = self.check_grammar(processed_prompts)
@@ -81,8 +101,9 @@ class PromptGenerator:
 
         t2 = time()
         duration = (t2 - t1) / 60.0
-        print(f"[INFO] It took: {colorama.Fore.GREEN}{duration}{colorama.Style.RESET_ALL} min.")
-        print("[INFO] Done.")
+        self.__logger.info(f" It took: {colorama.Fore.GREEN}{duration}{colorama.Style.RESET_ALL} min.")
+        self.__logger.info(" Done.")
+        print("\n")
 
     def check_prompt(self, prompt: str):
 
@@ -94,7 +115,7 @@ class PromptGenerator:
                      f"Perform contextual analysis check of the input prompt. If failed, score it the lowest. "
                      f"Check if all the words in the input prompt makes sense together and describe an object. If failed, score it the lowest. "
                      f"Check if the input prompt has a logic between the words. If failed, score it the lowest. "
-                     f"Check if the input prompt is finished and has an object or subject in it. If not, score it the lowest. "
+                     f"Check if the input prompt is finished and has an object or subject in it. If not, score it the lowest and ignore other checks. "
                      f"Check if all words in the prompt can be found in a dictionary. If not, score it the lowest. "
                      f"Use performed checks to score the input prompt between 0 (all checks are failed) and 1 (all checks passed). "
                      "You must keep answers short and concise. "
@@ -115,17 +136,18 @@ class PromptGenerator:
 
         return score
 
-
     """ llama-cpp loader for LLM models. LLM models should be stored in .gguf file format. """
     def offline_generator(self):
-        print("*" * 40)
-        print("[INFO] *** Prompt Dataset Generator ***")
-        print("*" * 40, "\n")
+        self.__logger.info("*" * 40)
+        self.__logger.info(" *** Prompt Dataset Generator ***")
+        self.__logger.info("*" * 40)
+        print("\n")
 
         model_path = self._load_offline_model()
 
         # init the llm model using Llama pipeline
-        print("[INFO] Preparing model.")
+        self.__logger.info(" Preparing model.")
+
         if self.__config_data['llm_model']['seed'] < 0:
             seed = random.randint(0, sys.maxsize)
         else:
@@ -138,11 +160,12 @@ class PromptGenerator:
                                     n_threads=self.__config_data['llm_model']['n_threads'],
                                     n_gpu_layers=self.__config_data['llm_model']['n_gpu_layers'],
                                     verbose=self.__config_data['llm_model']['verbose'])
-        print("[INFO] Done.\n")
+        self.__logger.info(" Done.")
+        print("\n")
 
         prompt = self._load_input_prompt()
         object_categories = self._load_object_categories()
-        print(f"[INFO] Object categories: {colorama.Fore.GREEN}{object_categories}{colorama.Style.RESET_ALL}")
+        self.__logger.info(f" Object categories: {colorama.Fore.GREEN}{object_categories}{colorama.Style.RESET_ALL}")
 
         # defining the grammar for the LLM model -> forcing to output strings according to specified rules
         grammar = llama_cpp.LlamaGrammar.from_string(r'''root ::= items
@@ -154,14 +177,17 @@ class PromptGenerator:
                                                           ''', verbose=self.__config_data['llm_model']['verbose'])
 
         # generate prompts using the provided object categories
-        print("[INFO] Started prompt generation.")
+        self.__logger.info(" Started prompt generation.")
         t1 = time()
         for i in range(self.__config_data["iteration_num"]):
-            print(f"\n[INFO] Iteration: {colorama.Fore.GREEN}{i}{colorama.Style.RESET_ALL} \n")
+            print("\n")
+            self.__logger.info(f" Iteration: {colorama.Fore.GREEN}{i}{colorama.Style.RESET_ALL}")
+            print("\n")
+
             output_list = []
             for category, _ in zip(object_categories, tqdm.trange(len(object_categories))):
                 temperature = random.uniform(0.45, 0.65)
-                # print(f"[INFO] Current temperature: {colorama.Fore.GREEN}{temperature}{colorama.Style.RESET_ALL}")
+                # self.__logger(f" Current temperature: {colorama.Fore.GREEN}{temperature}{colorama.Style.RESET_ALL}")
 
                 # find 'member' in the input string and replace it with category
                 prompt = prompt.replace("member_placeholder", category)
@@ -174,15 +200,19 @@ class PromptGenerator:
                 prompt = prompt.replace(category, "member_placeholder")
 
                 # extracting the response of the llm model: generated prompts
-                output_list.append(output['choices'][0]['text'])
+                output_prompt = output['choices'][0]['text']
+                score = self.check_prompt(output_prompt)
+                if float(score[0]) > 0.5:
+                    output_list.append(output_prompt)
 
             processed_prompts = self.post_process_prompts(output_list)
             self.save_prompts(processed_prompts)
 
         t2 = time()
         duration = (t2 - t1) / 60.0
-        print(f"[INFO] It took: {colorama.Fore.GREEN}{duration}{colorama.Style.RESET_ALL} min.")
-        print("[INFO] Done.")
+        self.__logger.info(f" It took: {colorama.Fore.GREEN}{duration}{colorama.Style.RESET_ALL} min.")
+        self.__logger.info(" Done.")
+        print("\n")
 
     """ Function for post processing the generated prompts. The LLM output is filtered from punctuation symbols and all non alphabetic characters.
     :param prompt_list: a list with strings (generated prompts)
@@ -207,16 +237,17 @@ class PromptGenerator:
 
     """ Function for filtering the prompts: removing prompts with certain words and prompts of certain length. """
     def filter_prompts(self):
-        print("*" * 40)
-        print("[INFO] *** Prompt Dataset Cleaner ***")
-        print("*" * 40, "\n")
+        self.__logger.info("*" * 40)
+        self.__logger.info(" *** Prompt Dataset Cleaner ***")
+        self.__logger.info("*" * 40)
+        print("\n")
 
         with open(self.__config_data["prompts_output_file"], "r") as file:
             prompts = [line.rstrip() for line in file]
             for i, p in enumerate(prompts):
                 prompts[i] = ' '.join(word.lower() for word in p.split())
 
-        print(f"[INFO] Total lines in the dataset before: {colorama.Fore.GREEN}{len(prompts)}{colorama.Style.RESET_ALL}")
+        self.__logger.info(f" Total lines in the dataset before: {colorama.Fore.GREEN}{len(prompts)}{colorama.Style.RESET_ALL}")
 
         articles = ["a", "the", "an"]
         prompts = [' '.join(word for word in sentence.split() if word.lower() not in articles) for sentence in prompts]
@@ -226,7 +257,9 @@ class PromptGenerator:
         prompts = [p for p in prompts if p not in self.__config_data["filter_colors"]]
         prompts = [l + "\n" if "\n" not in l else l for l in prompts]
 
-        print(f"[INFO] Total lines in the dataset after: {colorama.Fore.GREEN}{len(prompts)}{colorama.Style.RESET_ALL}")
+        self.__logger.info(f" Total lines in the dataset after: {colorama.Fore.GREEN}{len(prompts)}{colorama.Style.RESET_ALL}")
+        self.__logger.info(" Done.")
+        print("\n")
 
         self.save_prompts(prompts, "w")
 
@@ -234,9 +267,8 @@ class PromptGenerator:
     :param prompts: a list with strings (generated prompts)
     :return a list with processed prompts stored as strings.
     """
-    @staticmethod
-    def check_grammar(prompts: list):
-        print("[INFO] Performing spell check of the generated prompts")
+    def check_grammar(self, prompts: list):
+        self.__logger.info(" Performing spell check of the generated prompts.")
         t1 = time()
 
         sym_spell = SymSpell(max_dictionary_edit_distance=2, prefix_length=7)
@@ -260,7 +292,10 @@ class PromptGenerator:
 
         t2 = time()
         duration = (t2 - t1) / 60.0
-        print(f"[INFO] It took: {colorama.Fore.GREEN}{duration}{colorama.Style.RESET_ALL} min.")
+        self.__logger.info(f" It took: {colorama.Fore.GREEN}{duration}{colorama.Style.RESET_ALL} min.")
+        self.__logger.info(" Done.")
+        print("\n")
+
         return corrected_prompts
 
     """ Function for loading parameters for running the LLM """
@@ -300,10 +335,13 @@ class PromptGenerator:
         prompt = self.__config_data["prompt_groq"]
         prompt = prompt.replace("prompts_num", str(self.__config_data["prompts_num"]))
 
-        print("[INFO] Input prompt: ")
+        self.__logger.info(" Input prompt: ")
+
         regex = re.compile(r'(?<=[^.{}])(\.)(?![{}])'.format("e.g.", "e.g."))
         prompt_printing = regex.sub(r'\1\n', prompt)
-        print(f"{colorama.Fore.GREEN}{prompt_printing}{colorama.Style.RESET_ALL}")
+
+        self.__logger.info(f"{colorama.Fore.GREEN}{prompt_printing}{colorama.Style.RESET_ALL}")
+
         return prompt
 
     """Function for loading object categories from where the LLM will sample objects' names for generating prompts
@@ -317,19 +355,19 @@ class PromptGenerator:
     def _load_offline_model(self):
         # model to pick up from the hugging face (should have .gguf extension to run with llama)
         hf_model_repo = self.__config_data["hugging_face_repo"]
-        print(f"[INFO] Hugging Face repository: {colorama.Fore.GREEN}{hf_model_repo}{colorama.Style.RESET_ALL}")
+        self.__logger.info(f" Hugging Face repository: {colorama.Fore.GREEN}{hf_model_repo}{colorama.Style.RESET_ALL}")
 
         # the name of the file to be downloaded
         model_file_name = self.__config_data["llm_model_file_name"]
-        print(f"[INFO] LLM model to load: {colorama.Fore.GREEN}{model_file_name}{colorama.Style.RESET_ALL}")
+        self.__logger.info(f" LLM model to load: {colorama.Fore.GREEN}{model_file_name}{colorama.Style.RESET_ALL}")
 
         # cache folder where you want to store the downloaded model
         cache_folder = self.__config_data["cache_folder"]
         os.makedirs(cache_folder, exist_ok=True)
-        print(f"[INFO] LLM model will be stored here: {colorama.Fore.GREEN}{cache_folder}{colorama.Style.RESET_ALL}")
+        self.__logger.info(f" LLM model will be stored here: {colorama.Fore.GREEN}{cache_folder}{colorama.Style.RESET_ALL}")
 
         model_path = hf_hub_download(repo_id=hf_model_repo, filename=model_file_name, cache_dir=cache_folder, local_files_only=True)
-        print(f"[INFO] Downloaded model stored in: {colorama.Fore.GREEN}{model_path}{colorama.Style.RESET_ALL} \n")
+        self.__logger.info(f" Downloaded model stored in: {colorama.Fore.GREEN}{model_path}{colorama.Style.RESET_ALL} \n")
 
         return model_path
 
