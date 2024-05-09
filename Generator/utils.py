1f4a5ce5c9c5a7046cab0977e3cec3b515088042
tesha <tereshin.tesha@gmail.com>
2024-05-09 10:58:30 +0100

    [ADDED] utils file with functions for loading config data, saving/loading prompts

---
+++ Generator/utils.py
@@ -0,0 +1,31 @@
+import yaml
+
+
+def load_config_file():
+    """ Function for loading parameters for running the LLM
+    return loaded dictionary with data from the configuration file"""
+    with open("launching_config.yml", "r") as file:
+        config_data = yaml.safe_load(file)
+    return config_data
+
+
+def load_file_with_prompts(file_name: str):
+    """ Function for loading the prompts dataset for processing.
+
+    :param return: list with loaded prompts
+    """
+    with open(file_name, "r") as file:
+        prompts = [line.rstrip() for line in file]
+    return prompts
+
+
+def save_prompts(file_name: str, prompts_list: list, mode: str = "a"):
+    """ Function for saving the prompts stored in the prompts list
+
+    :param file_name: a string with the name of the file that will be loaded
+    :param prompts_list: a list with strings (generated prompts)
+    :param mode: mode for writing the file: 'a', 'w'
+    """
+    with open(file_name, mode) as file:
+        for p in prompts_list:
+            file.write("%s" % p)
