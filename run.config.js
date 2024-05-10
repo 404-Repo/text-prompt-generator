6aa18234a68e0ba70e23e1a730e8812ee99c99ca
tesha <tereshin.tesha@gmail.com>
2024-05-10 17:31:04 +0100

    [UPDATED] removed

--- run.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  apps : [{
-    name: 'prompts_generator',
-    script: 'propmpt_generator_server.py',
-    interpreter: '${CONDA_INTERPRETER_PATH}',
-    args: '--arg --arg --arg'
-  }]
-};
