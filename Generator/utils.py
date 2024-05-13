dd82d798a9bc994c9487638f92f3318a62c33334
tesha <tereshin.tesha@gmail.com>
2024-05-13 18:11:49 +0100

    [UPDATED] new tests + fixed old ones

--- Generator/utils.py
+++ Generator/utils.py
@@ -1,5 +1,8 @@
 import yaml
 
+from awq import AutoAWQForCausalLM
+from transformers import AutoTokenizer
+
 # import avro.schema as schema
 # from avro.datafile import DataFileReader, DataFileWriter
 # from avro.io import DatumReader, DatumWriter
@@ -33,6 +36,29 @@ def save_prompts(file_name: str, prompts_list: list, mode: str = "a"):
     with open(file_name, mode) as file:
         for p in prompts_list:
             file.write("%s" % p)
+
+
+def quantize_llm_awq(model_path_hf: str, quant_model_path: str):
+    """ Function for quantizing the LLM model using AWQ library
+
+    :param model_path_hf: path to the higging face library
+    :param quant_model_path: output path of the quantozed model
+    """
+    quant_config = {"zero_point": True,
+                    "q_group_size": 128,
+                    "w_bit": 4,
+                    "version": "GEMM"}
+
+    # Load model
+    model = AutoAWQForCausalLM.from_pretrained(model_path_hf)
+    tokenizer = AutoTokenizer.from_pretrained(model_path_hf, trust_remote_code=True)
+
+    # Quantize
+    model.quantize(tokenizer, quant_config=quant_config)
+
+    # Save quantized model
+    model.save_quantized(quant_model_path)
+    tokenizer.save_pretrained(quant_model_path)
 
 
 # def load_prompts_avro(file_name: str):
