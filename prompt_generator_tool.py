ea6b7cff1875cc8d1cf711ff02f85d8338ee939b
tesha <tereshin.tesha@gmail.com>
2024-08-13 15:37:16 +0100

    [UPDATED] updated prompts_checked -> stripped of redundant text

--- prompt_generator_tool.py
+++ prompt_generator_tool.py
@@ -94,9 +94,10 @@ def main():
 
                 prompt_checker.load_model("microsoft/Phi-3-small-128k-instruct")
                 prompts_checked = prompt_checker.check_prompts_for_completeness(prompts_dataset)
-                logger.info(f"{len(prompts_checked)} / {len(prompts_dataset)}")
                 logger.info(f"{prompts_checked} \n")
+                prompts_checked = prompts_checked.split("### Solution\n\n")[1].strip()
                 prompts_checked = [s.split('. ', 1)[1] for s in prompts_checked]
+                logger.info(f"{len(prompts_checked)} / {len(prompts_dataset)}")
                 logger.info(f"{prompts_checked}")
 
                 prompt_checker.unload_model()
