056886a5f5759c43b37d274cf151fe47b04db806
tesha <tereshin.tesha@gmail.com>
2024-08-15 10:07:11 +0100

    [ADDED] abstract class for generators backends

---
+++ generator/backend_generators/base_generator.py
@@ -0,0 +1,40 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+
+class BaseGenerator(ABC):
+    """
+    BaseGenerator is an abstract base class for defining different prompt generator backends.
+    Subclasses must implement methods for generating prompts.
+    """
+    @abstractmethod
+    def generate(self, instruction_prompt: str,  object_categories: List[str]):
+        """
+        Function that calls generator API for generating prompts.
+
+        Parameters
+        ----------
+        instruction_prompt: a string with instruction prompt
+        object_categories: a list of strings with categories
+
+        Returns
+        -------
+        output_prompts: list with generated prompts
+        """
+        pass
+
+    @abstractmethod
+    def preload_model(self, model_name: str):
+        """
+        Function for preloading LLM model in GPU memory
+
+        Parameters
+        ----------
+        model_name: the name of the model from the HF (hugging face)
+        """
+        pass
+
+    @abstractmethod
+    def unload_model(self):
+        """ Function for unloading the model """
+        pass
