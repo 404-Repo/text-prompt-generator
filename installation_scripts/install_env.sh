5ca5048bdbfd8299b99c5f6060a953bd29d33e6c
tesha <tereshin.tesha@gmail.com>
2024-08-19 16:10:12 +0100

    [REMOVED] updated package versions

--- installation_scripts/install_env.sh
+++ installation_scripts/install_env.sh
@@ -20,7 +20,7 @@ conda info --env
 
 CUDA_HOME=${CONDA_PREFIX}
 pip install flash-attn==2.6.1 --no-build-isolation
-pip install flashinfer==0.1.3 -i https://flashinfer.ai/whl/cu121/torch2.3
+pip install flashinfer==0.1.3 -i https://flashinfer.ai/whl/cu121/torch2.4
 
 # Store the path of the Conda interpreter
 CONDA_INTERPRETER_PATH=$(which python)
