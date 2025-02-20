2b29f577c75fb719b3dd905d0fb7beeb4ad3656f
Alexander Tereshin <31708304+teshaTe@users.noreply.github.com>
2025-02-20 16:46:23 +0000

    Pipeline update (#23)
    * [UPDATED] updated configs
    
    * [UPDATED] updated packages, poetry files
    
    * [UPDATED] added support for posting info to discord + doc strings
    
    * [UPDATED] check if the template is supported by the model
    
    * [UPDATED] updated handling model swap
    
    * [UPDATED] updated iteration number for swapping models

--- installation_scripts/install_runpod_env.sh
+++ installation_scripts/install_runpod_env.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
+conda init --all
 
 # Stop the script on any error
 set -e
 
 apt update
 apt install nano
-apt install vim npm -y
+apt install npm -y
 npm install pm2 -g
 npm fund
 
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O /opt/miniconda-installer.sh
-bash /opt/miniconda-installer.sh -b -p /opt/miniconda3
+mkdir -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm ~/miniconda3/miniconda.sh
 
-/opt/miniconda3/bin/conda init
+source ~/miniconda3/bin/activate
+conda init --all
+
--- prompt_generator_server.py
+++ prompt_generator_server.py
@@ -12,7 +12,9 @@ import requests
 import torch
 import tqdm
 import uvicorn
+from discord_webhook import DiscordWebhook
 from fastapi import FastAPI, Form
+from fastapi.responses import JSONResponse
 from generator.prompt_generator import PromptGenerator
 from loguru import logger
 
@@ -49,10 +51,12 @@ async def lifespan(app: FastAPI):  # type: ignore[no-untyped-def]
     logger.info("Pre-downloading all models.")
     # Startup logic
     current_dir = Path.cwd()
-    generator_config = io_utils.load_config_file(current_dir.resolve() / "configs" / "generator_config.yml")
-    llm_model = generator_config["vllm_api"]["llm_model"]
+    app.state.generator_config = io_utils.load_config_file(current_dir.resolve() / "configs" / "generator_config.yml")
+    # llm_model = generator_config["vllm_api"]["llm_model"]
     app.state.generator = PromptGenerator(args.backend)
-    app.state.generator.load_model(llm_model)
+    app.state.server_config = io_utils.load_config_file(current_dir.resolve() / "configs" / "server_config.yml")
+
+    # app.state.generator.load_model(llm_model)
 
     yield
 
@@ -109,18 +113,7 @@ def send_data_with_retry(config_data: dict, prompts_list: list[str], headers: di
     return False
 
 
-@app.post("/generate_prompts/")
-async def generate_prompts(save_locally_only: bool = Form(False)) -> None:
-    """
-    Server function for running the prompt generation
-    Parameters
-    ----------
-    inference_api: string with the name of the inference api that is supported by the generator:
-                   currently: groq, vllm
-    save_locally_only: enable/disable saving locally to the hdd only without posting to remote server
-
-    """
-
+def generation(save_locally_only: bool = False) -> None:
     logger.info("\n" + "*" * 35)
     logger.info(" *** Prompt Dataset generator ***")
     logger.info("*" * 35 + "\n")
@@ -128,8 +121,7 @@ async def generate_prompts(save_locally_only: bool = Form(False)) -> None:
     # loading server config
     current_dir = Path.cwd()
 
-    server_config = io_utils.load_config_file(current_dir.resolve() / "configs" / "server_config.yml")
-    headers = {"Content-Type": "application/json", "X-Api-Key": f'{server_config["api_key_prompt_server"]}'}
+    headers = {"Content-Type": "application/json", "X-Api-Key": f'{app.state.server_config["api_key_prompt_server"]}'}
 
     # defines whether we will have an infinite loop or not
     pipeline_config = io_utils.load_config_file(current_dir.resolve() / "configs" / "pipeline_config.yml")
@@ -143,13 +135,27 @@ async def generate_prompts(save_locally_only: bool = Form(False)) -> None:
         logger.info("Running infinite loop. Interrupt by CTRL + C.")
 
     # loop for prompt generation
+    j = 0
+    model_names = app.state.generator_config["vllm_api"]["llm_models"]
     prompts_to_send = []
+    change_model = True
     for i, _ in enumerate(total_iters):
+        # added loading of the model
+        if change_model:
+            ind = j % len(model_names)
+            logger.warning(f"########## Current model in use: [ {model_names[ind]} ] ##########")
+            app.state.generator.load_model(model_names[ind])
+            change_model = False
+
+            gc.collect()
+            torch.cuda.empty_cache()
+
         t1_local = time()
 
-        logger.info(f"Generation Iteration: {i}\n")
+        logger.info(f"********** Generation Iteration: {i} **********\n")
         prompts = app.state.generator.generate()
 
+        # filtering generated prompts
         prompts_out = prompt_filters.post_process_generated_prompts(prompts)
         prompts_out = prompt_filters.filter_unique_prompts(prompts_out)
         prompts_out = prompt_filters.filter_prompts_with_words(
@@ -160,29 +166,92 @@ async def generate_prompts(save_locally_only: bool = Form(False)) -> None:
         )
         prompts_out = prompt_filters.correct_non_finished_prompts(prompts_out)
 
+        # accumulating list with generated prompts
         prompts_to_send += prompts_out
+
+        # filter accumulated list
         prompts_to_send = prompt_filters.filter_unique_prompts(prompts_to_send)
 
         logger.info(f"Current prompts list size: {len(prompts_to_send)} / 1000+")
-
         io_utils.save_prompts(pipeline_config["prompts_output_file"], prompts_out)
 
         t2_local = time()
         iter_duration = (t2_local - t1_local) / 60.0
         logger.info(f"Current iteration took: {iter_duration} min.")
 
+        # posting accumulated prompts to the remote server with prompt validator
         if len(prompts_to_send) >= 1000:
             if not save_locally_only:
-                result = send_data_with_retry(server_config, prompts_to_send, headers)
+                result = send_data_with_retry(app.state.server_config, prompts_to_send, headers)
                 if result:
                     prompts_to_send.clear()
 
+        # every iteration load cached prompts -> filter unique ones -> save file
         if i % 500 == 0:
             prompts = io_utils.load_file_with_prompts(pipeline_config["prompts_output_file"])
             prompts_filtered = prompt_filters.filter_unique_prompts(prompts)
             io_utils.save_prompts(pipeline_config["prompts_output_file"], prompts_filtered, "w")
             prompts_to_send.clear()
             logger.info(f"Current dataset size: {len(prompts_filtered)}")
+
+        # unloading current model being in use to swap it with a new one on the next iteration
+        if i % pipeline_config["iterations_for_swapping_model"] == 0:
+            logger.warning("Unloading model before swapping ...")
+            app.state.generator.unload_model()
+            change_model = True
+            j += 1
+
+
+@app.post("/generate_prompts/")
+async def generate_prompts(save_locally_only: bool = Form(False), prompt_generator_number: int = Form(1)) -> None:
+    """
+    Server function for running the prompt generation
+    Parameters
+    ----------
+    inference_api: string with the name of the inference api that is supported by the generator:
+                   currently: groq, vllm
+    save_locally_only: enable/disable saving locally to the hdd only without posting to remote server
+
+    """
+    discord_message = f"🚨 **Prompt Generator {prompt_generator_number}** 🚨\n\n ONLINE"
+    webhook = DiscordWebhook(url=app.state.server_config["discord_webhook_url"], content=discord_message)
+    response = webhook.execute()
+    if response.status_code != 200:
+        logger.error(f"Failed to send message to Discord. Status code: {response.status_code}")
+
+    try:
+        generation(save_locally_only)
+        app.state.generator.unload_model()
+
+    except RuntimeError as e:
+        await post_exception_to_discord(str(e), prompt_generator_number)
+        raise e
+
+
+async def post_exception_to_discord(message: str, prompt_generator_number: int) -> JSONResponse:
+    # Log the error details
+    error_message = f"Server Error: {message}."
+
+    _, gpu_memory_total = torch.cuda.mem_get_info()
+    gpu_available_memory = gpu_memory_total - torch.cuda.memory_allocated()
+
+    logger.error(error_message)
+    logger.error(f" Total VRAM available: {gpu_available_memory / 1024 ** 3} Gb")
+    logger.error(f" Total VRAM allocated: {torch.cuda.memory_allocated() / 1024 ** 3} Gb")
+
+    # Send notification to Discord
+    discord_message = (
+        f"🚨 **Server Error Notification, PROMPT GENERATOR {prompt_generator_number}** 🚨\n\n"
+        f"**Error:** {error_message}\n"
+    )
+    webhook = DiscordWebhook(url=app.state.server_config["discord_webhook_url"], content=discord_message)
+    response = webhook.execute()
+
+    if response.status_code != 200:
+        logger.error(f"Failed to send message to Discord. Status code: {response.status_code}")
+
+    # Return JSON response to the client
+    return JSONResponse(status_code=500, content={"detail": "Internal Server Error: CUDA out of memory."})
 
 
 if __name__ == "__main__":
